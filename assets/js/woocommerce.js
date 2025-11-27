d28d5d2333934ae88df38f647579d4b125dabea9
JomariHinayon <hinayonjomari@gmail.com>
2025-11-27 16:56:05 +0800

    Changed the add to cart button and design base of the clients want

--- assets/js/woocommerce.js
+++ assets/js/woocommerce.js
@@ -19,6 +19,50 @@
             $(this).removeClass('hovered');
         });
         
+        $('.products').on('click', '.dashvio-qty__btn', function() {
+            var $btn = $(this);
+            var $quantityWrapper = $btn.siblings('.quantity');
+            var $input = $quantityWrapper.find('input.qty');
+            
+            if (!$input.length) {
+                return;
+            }
+            
+            var currentVal = parseFloat($input.val());
+            var max = parseFloat($input.attr('max'));
+            var min = parseFloat($input.attr('min'));
+            var step = parseFloat($input.attr('step'));
+            
+            if (isNaN(currentVal)) {
+                currentVal = 1;
+            }
+            if (isNaN(min)) {
+                min = 1;
+            }
+            if (isNaN(max)) {
+                max = Infinity;
+            }
+            if (isNaN(step) || step <= 0) {
+                step = 1;
+            }
+            
+            if ($btn.data('type') === 'plus') {
+                if (currentVal + step <= max) {
+                    $input.val(currentVal + step);
+                } else {
+                    $input.val(max);
+                }
+            } else {
+                if (currentVal - step >= min) {
+                    $input.val(currentVal - step);
+                } else {
+                    $input.val(min);
+                }
+            }
+            
+            $input.trigger('change');
+        });
+        
     });
 
 })(jQuery);
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -28,7 +28,33 @@ if (empty($product) || !$product->is_visible()) {
                 <span class="price"><?php echo $price_html; ?></span>
             <?php endif; ?>
             
-            <?php woocommerce_template_loop_add_to_cart(); ?>
+            <?php if ($product->is_type('simple') && $product->is_purchasable() && $product->is_in_stock()) : ?>
+                <form class="dashvio-loop-cart" action="<?php echo esc_url($product->add_to_cart_url()); ?>" method="post">
+                    <div class="dashvio-qty-control">
+                        <button type="button" class="dashvio-qty__btn" data-type="minus" aria-label="<?php esc_attr_e('Decrease quantity', 'dashvio'); ?>">-</button>
+                        <?php
+                        echo woocommerce_quantity_input(
+                            array(
+                                'input_name'  => 'quantity',
+                                'input_value' => 1,
+                                'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+                                'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+                                'step'        => apply_filters('woocommerce_quantity_input_step', 1, $product),
+                            ),
+                            $product,
+                            false
+                        );
+                        ?>
+                        <button type="button" class="dashvio-qty__btn" data-type="plus" aria-label="<?php esc_attr_e('Increase quantity', 'dashvio'); ?>">+</button>
+                    </div>
+                    <button type="submit" class="dashvio-loop-cart__btn">
+                        <?php echo esc_html($product->add_to_cart_text()); ?>
+                    </button>
+                    <input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>">
+                </form>
+            <?php else : ?>
+                <?php woocommerce_template_loop_add_to_cart(); ?>
+            <?php endif; ?>
         </div>
     </div>
 </li>
